88a532890e521481928954d808efc217b6f0ba7e
viv <131888240+vivek123r@users.noreply.github.com>
2024-07-24 21:34:14 +0530

    Create 4.c

---
+++ 4.c
@@ -0,0 +1,12 @@
+  GNU nano 6.2                                                                                      a.c                                                                                               #include <stdio.h>
+#include <unistd.h>
+int main()
+{
+    int x = 1;
+    int pid=fork();
+    if (pid == 0)
+        printf("Process id of Child is %d\n", getpid());
+    else
+        printf("Process id of parent is %d\n", getppid());
+    return 0;
+}
