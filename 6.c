6fe9b824787ecf043bb95c12f6dd91d22abb5d8c
viv <131888240+vivek123r@users.noreply.github.com>
2024-07-24 21:53:55 +0530

    Update 6.c

--- 6.c
+++ 6.c
@@ -1,1 +1,46 @@
+execl systemcall
+
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+#include<stdlib.h>
+int main()
+  {
+   int pid=fork();
+   printf("process created");
+	(execl("/bin/date","NULL"));
+   printf("execl executed");	
+    return(0);
+  }
+
+execv systemcall
+execsys.c
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    printf("Main pgm executing\n");
+    char *args[] = {"Hello", "C", "Programming", NULL};
+    execv("./hello", args);
+    printf("Back to main pgm");
+    return 0;
+}
+
+hello.c
+
+
+#include <unistd.h>
+#include <stdlib.h>
+int main(int argc, char *argv[])
+{
+    printf(" Hello.c executing\n");
+    printf("First content of passed array is %s\n", argv[0]);
+    return 0;
+}
+Execution format
+Gcc –o execsys execsys.c
+Gcc –o hello hello.c
+./execsys
 
